e80ee58bd811c062be6cc1e9e5f65e1bc5f4845a
Vinay <vinayvnair0@gmail.com>
2026-01-06 23:08:22 +0530

    Initial commit

--- reactnew/src/api/axios.js
+++ reactnew/src/api/axios.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: `${process.env.REACT_APP_API_URL}/api/`,
 });
 
 api.interceptors.request.use((config) => {
-  if (!config.url.includes("login")) {
+  if (!config.url?.includes("login")) {
     const token = localStorage.getItem("token");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
